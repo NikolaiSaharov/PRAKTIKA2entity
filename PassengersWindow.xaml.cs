4d0a740f41f44ab9b480d76fc63c90b5603a96da
NikolaiSaharov <145434551+NikolaiSaharov@users.noreply.github.com>
2024-03-21 09:44:17 +0300

    Add files via upload

---
+++ PassengersWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Windows;
+using System.Data.Entity;
+using System.Linq;
+using PRAKTIKA2;
+using System.Collections.ObjectModel;
+using System;
+using System.Windows.Controls;
+
+namespace PRAKTIKA2
+{
+    public partial class PassengersWindow : Window
+    {
+        private AVIACASSA2Entities1 dbContext;
+        
+        public PassengersWindow()
+        {
+            InitializeComponent();
+            dbContext = new AVIACASSA2Entities1();
+            LoadPassengers();
+        }
+        
+        private void LoadPassengers()
+        {
+            dbContext.Passengers.Load();
+            passengersDataGrid.ItemsSource = dbContext.Passengers.ToList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPreviousWindow();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            ShowNextWindow();
+        }
+
+        private void ShowPreviousWindow()
+        {
+            
+            this.Close();
+        }
+
+        private void ShowNextWindow()
+        {
+            TicketsWindow ticketWindow = new TicketsWindow();
+            ticketWindow.Show();
+            this.Close();
+        }
+
+        private void Dob(object sender, RoutedEventArgs e)
+        {
+            var passenger = new Passengers
+            {
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+                Email = Email.Text,
+                Phone = Phone.Text
+            };
+
+            dbContext.Passengers.Add(passenger);
+            dbContext.SaveChanges();
+            LoadPassengers();
+        }
+
+
+        private void Izm(object sender, RoutedEventArgs e)
+        {
+            if (passengersDataGrid.SelectedItem != null)
+            {
+                var selectedPassenger = (Passengers)passengersDataGrid.SelectedItem;
+                var passengerToUpdate = dbContext.Passengers.Find(selectedPassenger);
+                if (passengerToUpdate != null)
+                {
+                    passengerToUpdate.FirstName = FirstNameIzm.Text;
+                    passengerToUpdate.LastName = LastNameIzm.Text;
+                    passengerToUpdate.Email = EmailIzm.Text;
+                    passengerToUpdate.Phone = PhoneIzm.Text;
+
+                    dbContext.SaveChanges();
+                    LoadPassengers();
+                }
+            }
+        }
+
+        private void Del(object sender, RoutedEventArgs e)
+        {
+            if (passengersDataGrid.SelectedItem != null)
+            {
+                var selectedPassenger = (Passengers)passengersDataGrid.SelectedItem;
+                dbContext.Passengers.Remove(selectedPassenger);
+                dbContext.SaveChanges();
+                LoadPassengers();
+            }
+        }
+        private void passengersDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (passengersDataGrid.SelectedItem != null)
+            {
+                var selectedPassenger = (Passengers)passengersDataGrid.SelectedItem;
+                FirstNameIzm.Text = selectedPassenger.FirstName;
+                LastNameIzm.Text = selectedPassenger.LastName;
+                EmailIzm.Text = selectedPassenger.Email;
+                PhoneIzm.Text = selectedPassenger.Phone;
+            }
+        }
+    }
+}
---
+++ TicketsWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows;
+using System.Data.Entity;
+using static PRAKTIKA2.PassengersWindow;
+using System.Linq;
+using System;
+
+namespace PRAKTIKA2
+{
+    public partial class TicketsWindow : Window
+    {
+        private AVIACASSA2Entities1 dbContext;
+
+        public TicketsWindow()
+        {
+            InitializeComponent();
+            dbContext = new AVIACASSA2Entities1();
+            LoadTickets();
+        }
+
+        private void LoadTickets()
+        {
+            dbContext.Tickets.Load();
+            ticketsDataGrid.ItemsSource = dbContext.Tickets.ToList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPreviousWindow();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            ShowNextWindow();
+        }
+
+        private void ShowPreviousWindow()
+        {
+            PassengersWindow passWindow = new PassengersWindow();
+            passWindow.Show();
+            this.Close();
+        }
+
+        private void ShowNextWindow()
+        {
+            
+            this.Close();
+        }
+
+        private void Izm(object sender, RoutedEventArgs e)
+        {
+            if (ticketsDataGrid.SelectedItem != null)
+            {
+                var selectedTicket = (Tickets)ticketsDataGrid.SelectedItem;
+                selectedTicket.SeatNumber = SeatNumberIzm.Text;
+                selectedTicket.Class = ClassIzm.Text;
+                if (decimal.TryParse(PriceIzm.Text, out decimal price))
+                {
+                    selectedTicket.Price = price;
+                }
+                else
+                {
+                    MessageBox.Show("Введите целое число для деняк");
+                    return; 
+                }
+
+                dbContext.SaveChanges();
+                LoadTickets();
+            }
+        }
+
+        private void Dob(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(Price.Text, out decimal price))
+            {
+                var newTicket = new Tickets
+                {
+                    SeatNumber = SeatNumber.Text,
+                    Class = Class.Text,
+                    Price = price
+                };
+
+                dbContext.Tickets.Add(newTicket);
+                dbContext.SaveChanges();
+                LoadTickets();
+            }
+        }
+
+        private void Del(object sender, RoutedEventArgs e)
+        {
+            if (ticketsDataGrid.SelectedItem != null)
+            {
+                var selectedTicket = (Tickets)ticketsDataGrid.SelectedItem;
+                dbContext.Tickets.Remove(selectedTicket);
+                dbContext.SaveChanges();
+                LoadTickets(); 
+            }
+        }
+
+        private void ticketsDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ticketsDataGrid.SelectedItem != null)
+            {
+                var selectedTicket = (Tickets)ticketsDataGrid.SelectedItem;
+                SeatNumberIzm.Text = selectedTicket.SeatNumber;
+                ClassIzm.Text = selectedTicket.Class;
+                PriceIzm.Text = selectedTicket.Price.ToString();
+            }
+        }
+    }
+}
